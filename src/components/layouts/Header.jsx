daad924619a32ec103dfd2fbe7b2563cc709aed4
CamiloCuenca <camilocuenca1810@gmail.com>
2025-01-30 20:37:51 -0500

    feat: create Header component #2

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,18 +1,25 @@
-
-
-export default function Header(){
-    return(
-        <header className="bg-purple p-3 text-white justify-items-center">
+export default function Header() {
+    return (
+        <header className="bg-purple p-4 text-white">
             <nav>
-                <ul className="flex space-x-15 font-bold ">
-                    <li>About Me</li>
-                    <li>My Skills</li>
-                    <li>My Projects</li>
+                <ul className="flex space-x-15 justify-center font-bold">
+                    <li>
+                        <a href="#about" className="hover:text-purple-300 transition duration-300">
+                            About Me
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#skills" className="hover:text-purple-300 transition duration-300">
+                            My Skills
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#projects" className="hover:text-purple-300 transition duration-300">
+                            My Projects
+                        </a>
+                    </li>
                 </ul>
             </nav>
-
         </header>
     );
 }
-
-
