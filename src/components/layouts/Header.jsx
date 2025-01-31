01b92ddba5dfebeacfab23ae1dc6b2750867364c
CamiloCuenca <camilocuenca1810@gmail.com>
2025-01-30 20:32:24 -0500

    feat: abstract initial components #1 - Initial components modularized for reusability.

---
+++ src/components/layouts/Header.jsx
@@ -0,0 +1,18 @@
+
+
+export default function Header(){
+    return(
+        <header className="bg-purple p-3 text-white justify-items-center">
+            <nav>
+                <ul className="flex space-x-15 font-bold ">
+                    <li>About Me</li>
+                    <li>My Skills</li>
+                    <li>My Projects</li>
+                </ul>
+            </nav>
+
+        </header>
+    );
+}
+
+
