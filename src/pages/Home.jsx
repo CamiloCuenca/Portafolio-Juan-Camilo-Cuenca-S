582dd96ed098959d42dee9dd64dae732593385e2
CamiloCuenca <camilocuenca1810@gmail.com>
2025-02-09 20:42:29 -0500

    improve: improved the structure of the Home page #10

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,27 @@
-import HeroSection from '../components/layouts/HeroSection'
-import SocialLinks from '../components/layouts/SocialLinks'
+import HeroSection from '../components/layouts/HeroSection';
+import Skills from '../components/layouts/Skills';
+import SocialLinks from '../components/layouts/SocialLinks';
+import AboutMe from '../components/layouts/AboutMe';
 
-export default function Home(){
-
-    return(
+export default function Home() {
+    return (
         <>
-            <HeroSection></HeroSection>
-            <SocialLinks></SocialLinks>
+        <HeroSection></HeroSection>
+        <SocialLinks />
+        <div className="flex">
+            
+
+            
+        <AboutMe /> 
+                    <Skills />
+                    <div></div>
+                 
+          
+
+           
+        </div>
+      
         </>
     );
 }
+
