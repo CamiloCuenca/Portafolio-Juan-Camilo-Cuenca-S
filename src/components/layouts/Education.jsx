dfb4e94f388d4d7bd25a940e956f7ec478d5ab07
CamiloCuenca <camilocuenca1810@gmail.com>
2025-02-19 23:28:05 -0500

    add studies component #18

---
+++ src/components/layouts/Education.jsx
@@ -0,0 +1,22 @@
+export default function Education() {
+    return (
+        <div>
+                    <h2 className="text-2xl font-bold mb-3">Formación Académica</h2>
+
+      <div className="p-4 bg-white rounded-lg shadow-md">
+ 
+        <div className="mb-3">
+          <h3 className="text-lg font-semibold">Ingeniería en Sistemas y Computación</h3>
+          <p className="text-gray-600">Universidad del Quindío | 2021 - En curso</p>
+        </div>
+  
+        <div>
+          <h3 className="text-lg font-semibold">Diplomado en Desarrollo de Aplicaciones Móviles</h3>
+          <p className="text-gray-600">Universidad Tecnológica de Pereira - MinTic | 2021</p>
+          <p className="text-gray-500">(800 Horas)</p>
+        </div>
+      </div>
+      </div>
+    );
+  }
+  
