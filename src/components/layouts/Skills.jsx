be66127da4723119a0c501e552c95339f4629473
CamiloCuenca <camilocuenca1810@gmail.com>
2025-07-31 15:04:11 -0500

    feat: mejorar el diseño y la estructura de la sección "Acerca de mí" y ajustar el espaciado en la sección de habilidades

--- src/components/layouts/Skills.jsx
+++ src/components/layouts/Skills.jsx
@@ -97,7 +97,7 @@ export default function Skills() {
   return (
     <section id="skills" className="max-w-4xl mx-auto p-4 md:p-6">
       <h2 className="text-2xl font-bold text-center mb-8">Mis Skills</h2>
-      <div className="space-y-8 md:space-y-12">
+      <div className="space-y-8 md:space-y-10">
         {/* Languages */}
         <div className="bg-white p-4 md:p-6 rounded-lg shadow-2xl transition-shadow hover:shadow-xl ">
           <h3 className="text-lg font-semibold mb-4 text-gray-800">Languages</h3>
